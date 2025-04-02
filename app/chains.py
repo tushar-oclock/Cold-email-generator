49ba4a5b540cd69a97e02b962bc3ccffdc91212e
Tushar arora <61559891+tushar-oclock@users.noreply.github.com>
2025-04-02 15:48:27 +0530

    First

---
+++ app/chains.py
@@ -0,0 +1,61 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.3-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+        ### JOB DESCRIPTION:
+        {job_description}
+
+        ### INSTRUCTION:
+        You are Tushar, a motivated fresher eager to kickstart your career in the industry. 
+        You are reaching out to the client regarding the job mentioned above, highlighting your enthusiasm, relevant skills, 
+        and willingness to contribute. 
+        Emphasize how your fresh perspective, adaptability, and eagerness to learn can add value to their team. 
+        Also, if applicable, mention any projects, internships, or coursework that align with the job requirements.
+        Use the following links, if relevant, to showcase any prior work: {link_list}.
+        
+        Keep the email concise, professional, and confident.
+        Do not provide a preamble.
+        ### EMAIL (NO PREAMBLE):
+
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
